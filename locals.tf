c02eebe36b84bf02a2f39c91946775a3d9dc2ea3
AJLab-GH <ajammes.ftnt@gmail.com>
2024-06-10 10:14:03 -0600

    fixed implicit dependencies

--- locals.tf
+++ locals.tf
@@ -2,19 +2,23 @@ locals {
 
   resource_group_name = "${var.prefix}-rg"
   location            = "canadacentral"
+  environment_tag     = "${var.prefix}-cFOS-Demo"
 
   resource_groups = {
-    local.resource_group_name = {
+    (local.resource_group_name) = {
       name     = local.resource_group_name
       location = local.location
+      tags = {
+        Environment = local.environment_tag
+      }
     }
   }
 
   kubernetes_clusters = {
     "${var.prefix}-aks-01" = {
       name                = "${var.prefix}-aks-01"
-      location            = local.location
-      resource_group_name = azurerm_resource_group.resource_groups[local.resource_group_name].name
+      location            = azurerm_resource_group.resource_group[local.resource_group_name].location
+      resource_group_name = azurerm_resource_group.resource_group[local.resource_group_name].name
       dns_prefix          = "${var.prefix}-aks-01"
 
       default_node_pool = {
--- resource_resource_group.tf
+++ resource_resource_group.tf
@@ -2,6 +2,7 @@ resource "azurerm_resource_group" "resource_group" {
   for_each = local.resource_groups
   name     = each.value.name
   location = each.value.location
+  tags     = each.value.tags
 }
 
 output "resource_groups" {
