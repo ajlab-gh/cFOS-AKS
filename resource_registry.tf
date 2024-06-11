9bc3b9402fa58fa52cd422e7324be80765f89420
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-06-11 12:25:22 -0400

    added registry.tf

---
+++ resource_registry.tf
@@ -0,0 +1,26 @@
+resource "azurerm_container_registry" "container-registry" {
+  for_each            = local.kubernetes_clusters
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  sku                           = "Premium"
+  admin_enabled                 = true
+  public_network_access_enabled = false
+  anonymous_pull_enabled        = false
+}
+
+#output "registry_login_server" {
+#  value = data.azurerm_container_registry.container-registry.login_server
+#}
+#output "registry_admin_username" {
+#  value = data.azurerm_container_registry.container-registry.admin_username
+#}
+#output "registry_admin_password" {
+#  value = data.azurerm_container_registry.container-registry.admin_password
+#}
+
+#data "azurerm_container_registry" "container-registry" {
+#  depends_on          = [azurerm_container_registry.container-registry]
+#  name                = random_pet.admin_username.id
+#  resource_group_name = azurerm_resource_group.azure_resource_group.name
+#}
