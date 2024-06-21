0f4f87ce0b1d56ba255be186507bb16e3ce29cef
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-06-21 12:45:09 -0400

    fix: setting all names to cfos instead of fos

--- terraform/resource_kubernetes_cluster.tf
+++ terraform/resource_kubernetes_cluster.tf
@@ -73,50 +73,50 @@ resource "azurerm_kubernetes_cluster_extension" "flux-extension" {
   extension_type = "microsoft.flux"
 }
 
-resource "azurerm_kubernetes_flux_configuration" "store-main" {
-  for_each   = local.kubernetes_clusters
-  name       = "store-main"
-  cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
-  namespace  = "flux-system"
-  scope      = "cluster"
-  git_repository {
-    url             = "https://github.com/AJLab-GH/cFOS-AKS"
-    reference_type  = "branch"
-    reference_value = "main"
-  }
-  kustomizations {
-    name                       = "main"
-    recreating_enabled         = true
-    garbage_collection_enabled = true
-    path                       = "./manifests/overlays/store-main"
-    sync_interval_in_seconds   = 60
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster_extension.flux-extension
-  ]
-}
-resource "azurerm_kubernetes_flux_configuration" "store-dev" {
-  for_each   = local.kubernetes_clusters
-  name       = "store-dev"
-  cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
-  namespace  = "flux-system"
-  scope      = "cluster"
-  git_repository {
-    url             = "https://github.com/AJLab-GH/cFOS-AKS"
-    reference_type  = "branch"
-    reference_value = "dev"
-  }
-  kustomizations {
-    name                       = "dev"
-    recreating_enabled         = true
-    garbage_collection_enabled = true
-    path                       = "./manifests/overlays/store-dev"
-    sync_interval_in_seconds   = 60
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster_extension.flux-extension
-  ]
-}
+#resource "azurerm_kubernetes_flux_configuration" "store-main" {
+#  for_each   = local.kubernetes_clusters
+#  name       = "store-main"
+#  cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
+#  namespace  = "flux-system"
+#  scope      = "cluster"
+#  git_repository {
+#    url             = "https://github.com/AJLab-GH/cFOS-AKS"
+#    reference_type  = "branch"
+#    reference_value = "main"
+#  }
+#  kustomizations {
+#    name                       = "main"
+#    recreating_enabled         = true
+#    garbage_collection_enabled = true
+#    path                       = "./manifests/overlays/store-main"
+#    sync_interval_in_seconds   = 60
+#  }
+#  depends_on = [
+#    azurerm_kubernetes_cluster_extension.flux-extension
+#  ]
+#}
+#resource "azurerm_kubernetes_flux_configuration" "store-dev" {
+#  for_each   = local.kubernetes_clusters
+#  name       = "store-dev"
+#  cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
+#  namespace  = "flux-system"
+#  scope      = "cluster"
+#  git_repository {
+#    url             = "https://github.com/AJLab-GH/cFOS-AKS"
+#    reference_type  = "branch"
+#    reference_value = "dev"
+#  }
+#  kustomizations {
+#    name                       = "dev"
+#    recreating_enabled         = true
+#    garbage_collection_enabled = true
+#    path                       = "./manifests/overlays/store-dev"
+#    sync_interval_in_seconds   = 60
+#  }
+#  depends_on = [
+#    azurerm_kubernetes_cluster_extension.flux-extension
+#  ]
+#}
 resource "azurerm_kubernetes_flux_configuration" "fos-dev" {
   for_each   = local.kubernetes_clusters
   name       = "fos-dev"
